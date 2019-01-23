18aa74e28a5b53c55207eb218172782c08764f15
Tom Claudio <>
2019-01-23 17:03:21 +0800

    Create tests and calculator functions

--- calculateTotal.js
+++ calculateTotal.js
@@ -1,13 +1,21 @@
 module.exports = {
-  calculateTotal: () => {
+  calculateTotal: (input) => {
+    let adult_total = input.adult[0]*input.adult[1]
+    let k02_total = input.k02[0]*input.k02[1] 
+    let k35_total = input.k35[0]*input.k35[1]
+    let k61_total = input.k61[0]*input.k61[1] 
+    let total = adult_total + k02_total + k35_total + k61_total
+    let downpayment = total/2
+    let balance = total - downpayment
+    
     computedValues = {
-      "adult_total": 0,
-      "k02_total": 0,
-      "k35_total": 0,
-      "k61_total": 0,
-      "downpayment": 0,
-      "total": 0,
-      "balance": 0,
+      "adult_total": adult_total,
+      "k02_total": k02_total,
+      "k35_total": k35_total,
+      "k61_total": k61_total,
+      "downpayment": downpayment,
+      "total": total,
+      "balance": balance,
     }
     return computedValues
   }
--- test/test_compute.js
+++ test/test_compute.js
@@ -2,8 +2,9 @@ const assert = require('chai').assert
 const expect = require('chai').expect
 const calculateTotal = require('../calculateTotal.js').calculateTotal
 const requiredReturnKeys = require('../Configurations.js').requiredReturnKeys
+const exampleInputValues = require('../Configurations.js').exampleInputValues
 
-var returnValue = calculateTotal()
+var returnValue = calculateTotal(exampleInputValues)
 
 describe('Compute', ()=> {
   it("Module Imported", ()=> {
@@ -16,6 +17,32 @@ describe('Compute', ()=> {
     it("Returned required fields", () => {
       expect(returnValue).to.have.keys(requiredReturnKeys)
     })
+  })
+  describe("Computations are correct", () => {
+    let total = (4*400) + (3*300) + (2*200) + (1*100)
+    let downpayment = total/2
+    let balance = total - downpayment
+    it("Adult total is correct", () => {
+      expect(returnValue.adult_total).to.be.equal(4*400)
+    })
+    it("k02 total is correct", () => {
+      expect(returnValue.k02_total).to.be.equal(1*100)
+    })
+    it("k35 total is correct", () => {
+      expect(returnValue.k35_total).to.be.equal(2*200)
+    })
+    it("k61 total is correct", () => {
+      expect(returnValue.k61_total).to.be.equal(3*300)
+    })
+    it("Total is correct", () => {
+      expect(returnValue.total).to.be.equal(total)
+    })
+    it("Downpayment is correct", () => {
+      expect(returnValue.downpayment).to.be.equal(downpayment)
+    })
+    it("Balance is correct", () => {
+      expect(returnValue.balance).to.be.equal(balance)
+    })           
   })
 })
 
