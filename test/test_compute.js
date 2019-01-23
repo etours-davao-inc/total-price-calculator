6b9db84ec5346ac22e9a98894a5c205a8fa41398
Tom Claudio <>
2019-01-23 16:34:32 +0800

    Refactor and move config to Configurations files

--- test/test_compute.js
+++ test/test_compute.js
@@ -1,17 +1,9 @@
 const assert = require('chai').assert
 const expect = require('chai').expect
 const calculateTotal = require('../calculateTotal.js').calculateTotal
+const requiredReturnKeys = require('../Configurations.js').requiredReturnKeys
 
 var returnValue = calculateTotal()
-var requiredReturnKeys = [
-  'adult_total',
-  'k02_total',
-  'k35_total',
-  'k61_total',
-  'downpayment',
-  'total',
-  'balance'
-]
 
 describe('Compute', ()=> {
   it("Module Imported", ()=> {
