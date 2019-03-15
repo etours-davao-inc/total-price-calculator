7e2ba071078e6ade2901e1b000efd47da7e9f6fc
Tom Claudio <>
2019-03-15 19:38:19 +0800

    Create input and output values

--- Configurations.js
+++ Configurations.js
@@ -1,17 +1,51 @@
 module.exports = {
-  requiredReturnKeys: [
-    'adult',
-    'kid02',
-    'kid35',
-    'kid611',
-    'downpayment',
-    'total',
-    'balance'
-  ],
-  exampleInputValues : {
+  input1 : {
     adult: [4,400],
     kid02: [1,100],
     kid35: [2,200],
     kid611: [3,300],
-  }
+  },
+  output1 : { 
+    total: 3000,
+    downpayment: 1500,
+    balance: 1500,
+    adult: 1600,
+    kid02: 100,
+    kid35: 400,
+    kid611: 900 
+  },
+  input2 : {
+    adult: [4,400],
+    kid02: [1,100],
+    kid35: [2,200],
+    kid611: [3,0],    
+  },
+  output2 : { 
+    total: 2900,
+    downpayment: 1450,
+    balance: 1450,
+    adult: 1600,
+    kid02: 100,
+    kid35: 400,
+    kid611: 800 
+  },  
+  input3 : {
+    basePrice: {
+      adult: [4,400],
+      kid02: [1,100],
+      kid35: [2,200],
+      kid611: [3,0],
+    },
+    hotelNights: 2,
+    hotelPricePerNight: 500.00  
+  },
+  output3 : { 
+    total: 4400,
+    downpayment: 2200,
+    balance: 2200,
+    adult: 2600,
+    kid02: 100,
+    kid35: 400,
+    kid611: 1300 
+  },  
 }
