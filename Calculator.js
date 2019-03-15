da40d975304c54c6f77e101744ffa42014855baa
Tom Claudio <>
2019-03-15 16:41:13 +0800

    Refactor code

--- Calculator.js
+++ Calculator.js
@@ -1,40 +1,25 @@
 
+// InputValues : {
+//  adult: [4,400],
+//  kid02: [1,100],
+//  kid35: [2,200],
+//  kid611: [3,300],
+// }
 const calculateTotal = (input) => {
-  let adult = input.adult[0]*input.adult[1]
-  let kid02 = input.k02[0]*input.k02[1] 
-  let kid35 = input.k35[0]*input.k35[1]
-  let kid611 = input.k61[0]*input.k61[1] 
-  let total = adult + kid02 + kid35 + kid611
-  let downpayment = total/2
-  let balance = total - downpayment
-  
-  let result = {
-    "adult": adult,
-    "kid02": kid02,
-    "kid35": kid35,
-    "kid611": kid611,
-    "downpayment": downpayment,
-    "total": total,
-    "balance": balance,
-    }
-    
-  return result
+  // Initialize Return Object Data Structure
+  let returnObject = {total: 0, downpayment: 0, balance: 0}
+  let objectKeys = Object.keys(input)
+  // Iterates input & do computation on each item
+  // Aggregate total, downpayment, balance
+  objectKeys.forEach((key)=> {
+    let item = input[key]
+    let product = item[0] * item[1]
+    returnObject[key] = product
+    returnObject.total += product
+    returnObject.downpayment = returnObject.total/2
+    returnObject.balance = returnObject.total - returnObject.downpayment
+  }) 
+  return returnObject
 }
 
-const calculatePriceWithHotel = (input) => {
-  let firstLevelKeys = ["basePrice", "hotelNights", "hotelPricePerNight"]
-
-  firstLevelKeys.forEach((key) => {
-    if (!(input.hasOwnProperty(key))) {
-      throw "Incomplete input parameters"
-    }
-  })
-
-  return {
-    "adult": 0,
-    "kid02": 0,
-    "kid35": 0,
-    "kid611": 0,
-  }
-}
-export {calculateTotal, calculatePriceWithHotel};
+export {calculateTotal};
