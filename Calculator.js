6b299faae873449048c238d806139cb0a342178c
Tom Claudio <>
2019-03-15 19:39:10 +0800

    Refactor; create if hotel is present

--- Calculator.js
+++ Calculator.js
@@ -4,11 +4,25 @@
 //  kid35: [2,200],
 //  kid611: [3,300],
 // }
+
+const Calculate = (input) => {
+  if (input.hotelNights) {
+    return calculateTotal2(input)
+  } else {
+    return calculateTotal(input)
+  }
+}
+
 const calculateTotal = (input) => {
   let adult = input.adult[0]*input.adult[1]
   let kid02 = input.kid02[0]*input.kid02[1] 
   let kid35 = input.kid35[0]*input.kid35[1]
-  let kid611 = input.kid611[0]*input.kid611[1] 
+  let kid611
+  if (input.kid611[1] > 0) {
+    kid611 = input.kid611[0]*input.kid611[1] 
+  } else {
+    kid611 = adult/2
+  }
   let total = adult + kid02 + kid35 + kid611
   let downpayment = total/2
   let balance = total - downpayment
@@ -22,8 +36,34 @@ const calculateTotal = (input) => {
     "total": total,
     "balance": balance,
     }
-    
   return result
 }
 
-export {calculateTotal};
+const calculateTotal2 = (input) => {
+  let totalHotelPrice = input.hotelNights * input.hotelPricePerNight
+  let adult = input.basePrice.adult[0]*input.basePrice.adult[1] + totalHotelPrice
+  let kid02 = input.basePrice.kid02[0]*input.basePrice.kid02[1] 
+  let kid35 = input.basePrice.kid35[0]*input.basePrice.kid35[1]
+  let kid611
+  if (input.basePrice.kid611[1] > 0) {
+    kid611 = input.basePrice.kid611[0]*input.basePrice.kid611[1] 
+  } else {
+    kid611 = adult/2
+  }
+  let total = adult + kid02 + kid35 + kid611
+  let downpayment = total/2
+  let balance = total - downpayment
+  
+  let result = {
+    "adult": adult,
+    "kid02": kid02,
+    "kid35": kid35,
+    "kid611": kid611,
+    "downpayment": downpayment,
+    "total": total,
+    "balance": balance,
+    }
+  return result
+}
+
+export {Calculate};
