3a989452299ceda6e0002c0628519999af6333ab
Tom Claudio <>
2019-03-18 16:42:43 +0800

    Rename adult to adults

--- Calculator.js
+++ Calculator.js
@@ -1,5 +1,5 @@
 // InputValues : {
-//  adult: [4,400],
+//  adults: [4,400],
 //  kid02: [1,100],
 //  kid35: [2,200],
 //  kid611: [3,300],
@@ -14,21 +14,21 @@ const Calculate = (input) => {
 }
 
 const calculate = (input) => {
-  let adult = input.adult[0]*input.adult[1]
+  let adults = input.adults[0]*input.adults[1]
   let kid02 = input.kid02[0]*input.kid02[1] 
   let kid35 = input.kid35[0]*input.kid35[1]
   let kid611
   if (input.kid611[1] > 0) {
     kid611 = input.kid611[0]*input.kid611[1] 
   } else {
-    kid611 = adult/2
+    kid611 = adults/2
   }
-  let total = adult + kid02 + kid35 + kid611
+  let total = adults + kid02 + kid35 + kid611
   let downpayment = total/2
   let balance = total - downpayment
   
   let result = {
-    "adult": adult,
+    "adults": adults,
     "kid02": kid02,
     "kid35": kid35,
     "kid611": kid611,
@@ -41,21 +41,21 @@ const calculate = (input) => {
 
 const calculateWithHotel = (input) => {
   let totalHotelPrice = input.hotelNights * input.hotelPricePerNight
-  let adult = input.basePrice.adult[0]*input.basePrice.adult[1] + totalHotelPrice
+  let adults = input.basePrice.adults[0]*input.basePrice.adults[1] + totalHotelPrice
   let kid02 = input.basePrice.kid02[0]*input.basePrice.kid02[1] 
   let kid35 = input.basePrice.kid35[0]*input.basePrice.kid35[1]
   let kid611
   if (input.basePrice.kid611[1] > 0) {
     kid611 = input.basePrice.kid611[0]*input.basePrice.kid611[1] 
   } else {
-    kid611 = adult/2
+    kid611 = adults/2
   }
-  let total = adult + kid02 + kid35 + kid611
+  let total = adults + kid02 + kid35 + kid611
   let downpayment = total/2
   let balance = total - downpayment
   
   let result = {
-    "adult": adult,
+    "adults": adults,
     "kid02": kid02,
     "kid35": kid35,
     "kid611": kid611,
