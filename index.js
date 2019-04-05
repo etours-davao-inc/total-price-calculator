bd274c732c7b4f09ded4ea95fbe720a90c55632f
Tom Claudio <>
2019-04-05 16:48:17 +0800

    Pass test from updated input variables

--- index.js
+++ index.js
@@ -40,13 +40,13 @@ const calculate = (input) => {
 }
 
 const calculateWithHotel = (input) => {
-  let totalHotelPrice = input.hotelNights * input.hotelPrice
-  let adults = input.basePrice.adults[0]*input.basePrice.adults[1] + totalHotelPrice
-  let kid02 = input.basePrice.kid02[0]*input.basePrice.kid02[1] 
-  let kid35 = input.basePrice.kid35[0]*input.basePrice.kid35[1]
+  let totalHotelPrice = input.hotelNights * input.hotel.price
+  let adults = input.adults[0]*input.adults[1] + totalHotelPrice
+  let kid02 = input.kid02[0]*input.kid02[1] 
+  let kid35 = input.kid35[0]*input.kid35[1]
   let kid611
-  if (input.basePrice.kid611[1] > 0) {
-    kid611 = input.basePrice.kid611[0]*input.basePrice.kid611[1] 
+  if (input.kid611[1] > 0) {
+    kid611 = input.kid611[0]*input.kid611[1] 
   } else {
     kid611 = adults/2
   }
