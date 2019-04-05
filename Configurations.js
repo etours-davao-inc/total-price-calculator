d9d20bd5d91611561c0378cc495f208df69421eb
Tom Claudio <>
2019-04-05 22:06:43 +0800

    Reconfigure test values to match change in structure

--- Configurations.js
+++ Configurations.js
@@ -6,13 +6,22 @@ module.exports = {
     kid611: [3,300],
   },
   output1 : { 
-    total: 3000,
-    downpayment: 1500,
-    balance: 1500,
-    adults: 1600,
-    kid02: 100,
-    kid35: 400,
-    kid611: 900 
+    item: { 
+      adults: 400,
+      kid02: 100,
+      kid35: 200,
+      kid611: 300
+    },
+    total: {
+      adults: 1600,
+      kid02: 100,
+      kid35: 400,
+      kid611: 900,
+      total: 3000,
+      downpayment: 1500,
+      balance: 1500,      
+    }
+
   },
   input2 : {
     adults: [4,400],
@@ -20,14 +29,22 @@ module.exports = {
     kid35: [2,200],
     kid611: [3,0],    
   },
-  output2 : { 
-    total: 2900,
-    downpayment: 1450,
-    balance: 1450,
-    adults: 1600,
-    kid02: 100,
-    kid35: 400,
-    kid611: 800 
+  output2 : {
+    item: { 
+      adults: 400,
+      kid02: 100,
+      kid35: 200,
+      kid611: 200
+    },    
+    total: {
+      adults: 1600,
+      kid02: 100,
+      kid35: 400,
+      kid611: 600,
+      total: 2700,
+      downpayment: 1350,
+      balance: 1350      
+    }
   },  
   input3 : {
     adults: [4,400],
@@ -41,13 +58,21 @@ module.exports = {
       price: 500.00  
     }
   },
-  output3 : { 
-    total: 4400,
-    downpayment: 2200,
-    balance: 2200,
-    adults: 2600,
-    kid02: 100,
-    kid35: 400,
-    kid611: 1300 
+  output3 : {
+    item: { 
+      adults: 1400,
+      kid02: 100,
+      kid35: 200,
+      kid611: 700
+    },      
+    total: {
+      adults: 5600,
+      kid02: 100,
+      kid35: 400,
+      kid611: 2100,
+      total: 8200,
+      downpayment: 4100,
+      balance: 4100
+    }
   },  
 }
