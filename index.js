0dd46c3e4739d6ce066207a7d22ef4e7f685c97b
Tom Claudio <>
2019-04-05 22:25:34 +0800

    Refactor calculator - improve maintainability

--- index.js
+++ index.js
@@ -1,71 +1,49 @@
-// InputValues : {
-//  adults: [4,400],
-//  kid02: [1,100],
-//  kid35: [2,200],
-//  kid611: [3,300],
-// }
-
-const Calculate = (input) => {
-  if (input.hotel) {
-    return calculateWithHotel(input)
-  } else {
-    return calculate(input)
-  }
-}
-
-const calculate = (input) => {
-  let adults = input.adults[0] * input.adults[1]
+export const Calculate = (input) => {
+  let {adults_item, adults_total} = setAdultItemAndTotal(input)
   let kid02 = input.kid02[0] * input.kid02[1]
   let kid35 = input.kid35[0] * input.kid35[1]
-  let kid611 = 0
+  let kid611_item; 
+  let kid611_total;
   if ((input.kid611[1] > 0) && (input.kid611[0] > 0)) {
-    kid611 = input.kid611[0] * input.kid611[1]
+    kid611_item = input.kid611[1]
+    kid611_total = input.kid611[0] * kid611_item
   } else if (input.kid611[0] > 0) {
-    kid611 = adults / 2
+    kid611_item = adults_item / 2
+    kid611_total = input.kid611[0] * kid611_item
   }
-  let total = adults + kid02 + kid35 + kid611
+  let total = adults_total + kid02 + kid35 + kid611_total
   let downpayment = total / 2
   let balance = total - downpayment
 
   let result = {
-    "adults": adults,
-    "kid02": kid02,
-    "kid35": kid35,
-    "kid611": kid611,
-    "downpayment": downpayment,
-    "total": total,
-    "balance": balance,
+    item: {
+      adults: adults_item,
+      kid02: input.kid02[1],
+      kid35: input.kid35[1],
+      kid611: kid611_item
+    },
+    total: {
+      adults: adults_total,
+      kid02: kid02,
+      kid35: kid35,
+      kid611: kid611_total,
+      downpayment: downpayment,
+      total: total,
+      balance: balance
+    }
   }
-  // console.log(result)
   return result
 }
 
-const calculateWithHotel = (input) => {
-  let totalHotelPrice = input.tourDates.hotelNights * input.hotel.price
-  let adults = input.adults[0] * input.adults[1] + totalHotelPrice
-  let kid02 = input.kid02[0] * input.kid02[1]
-  let kid35 = input.kid35[0] * input.kid35[1]
-  let kid611
-  if (input.kid611[1] > 0) {
-    kid611 = input.kid611[0] * input.kid611[1]
+const setAdultItemAndTotal = (input) => {
+  let returnItems = {};
+  if (input.hotel) {
+    let totalHotelPrice = input.tourDates.hotelNights * input.hotel.price
+    returnItems.adults_item = input.adults[1] + totalHotelPrice
   } else {
-    kid611 = adults / 2
+    returnItems.adults_item = input.adults[1]
   }
-  let total = adults + kid02 + kid35 + kid611
-  let downpayment = total / 2
-  let balance = total - downpayment
-
-  let result = {
-    "adults": adults,
-    "kid02": kid02,
-    "kid35": kid35,
-    "kid611": kid611,
-    "downpayment": downpayment,
-    "total": total,
-    "balance": balance,
-  }
-  console.log(result)
-  return result
+  returnItems.adults_total = input.adults[0] * returnItems.adults_item
+  return returnItems;
 }
 
-export { Calculate };
