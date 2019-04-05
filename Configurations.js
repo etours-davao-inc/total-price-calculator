3121fc3c0808f898059b3d53fd705e82b85b6d83
Tom Claudio <>
2019-04-05 16:33:46 +0800

    Update adult key to adults

--- Configurations.js
+++ Configurations.js
@@ -1,6 +1,6 @@
 module.exports = {
   input1 : {
-    adult: [4,400],
+    adults: [4,400],
     kid02: [1,100],
     kid35: [2,200],
     kid611: [3,300],
@@ -9,13 +9,13 @@ module.exports = {
     total: 3000,
     downpayment: 1500,
     balance: 1500,
-    adult: 1600,
+    adults: 1600,
     kid02: 100,
     kid35: 400,
     kid611: 900 
   },
   input2 : {
-    adult: [4,400],
+    adults: [4,400],
     kid02: [1,100],
     kid35: [2,200],
     kid611: [3,0],    
@@ -24,14 +24,14 @@ module.exports = {
     total: 2900,
     downpayment: 1450,
     balance: 1450,
-    adult: 1600,
+    adults: 1600,
     kid02: 100,
     kid35: 400,
     kid611: 800 
   },  
   input3 : {
     basePrice: {
-      adult: [4,400],
+      adults: [4,400],
       kid02: [1,100],
       kid35: [2,200],
       kid611: [3,0],
@@ -43,7 +43,7 @@ module.exports = {
     total: 4400,
     downpayment: 2200,
     balance: 2200,
-    adult: 2600,
+    adults: 2600,
     kid02: 100,
     kid35: 400,
     kid611: 1300 
